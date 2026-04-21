8d7b4a7329c20670e7abd8a2faa62beec267be9a
Nieves Alonso Gilsanz <nievalon@ucm.es>
2026-04-21 14:19:55 +0200

    raytracing

---
+++ Raytracing/03_raytracerArregladosErrores/raytracer/Color.h
@@ -0,0 +1,15 @@
+#ifndef __COLOR_H
+#define __COLOR_H
+
+#include "glm/vec3.hpp"
+
+
+using Color = glm::vec3;
+
+// Constantes de colores
+static Color RED = Color(1, 0, 0);
+static Color BLACK = Color(0, 0, 0);
+static Color WHITE = Color(1, 1, 1);
+
+
+#endif
---
+++ Raytracing/03_raytracerArregladosErrores/raytracer/main.cpp
@@ -0,0 +1,49 @@
+#include "glm/vec3.hpp"
+#include "glm/geometric.hpp"
+#include "Film.h"
+#include "Color.h"
+#include "camera.hpp"
+
+#include <fstream>
+
+const glm::vec3 sphere_position{0.0, 0.0, 1.0};
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    glm::vec3 oc = center - r.origin();
+    auto a = glm::dot(r.direction(), r.direction());
+    auto b = -2.0 * glm::dot(r.direction(), oc);
+    auto c = glm::dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+
+Color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+        return Color(1, 0, 0);
+
+    glm::vec3 unit_direction = glm::normalize(r.direction());
+    float a = 0.5*(unit_direction.y + 1.0);
+    return (1.0f-a)*Color(1.0, 1.0, 1.0) + a*Color(0.5, 0.7, 1.0);
+}
+
+int main(void) {
+    std::ofstream out{"imagen.ppm"};
+    Film film{800, 600, out};
+    glm::vec3 unit_direction{};
+
+    const camera cam{
+        {0.0, 0.0, 0.0},
+        sphere_position,
+        {0.0, 1.0, 0.0},
+        film,
+        90.0
+    };
+
+    for (std::size_t y = 0; y < film.GetTamY(); ++y) {
+        for (std::size_t x = 0; x < film.GetTamX(); ++x) {
+            const ray ray_primary = cam.get_ray(x, y);
+            const Color c = ray_color(ray_primary);
+            film.AddPixel(c);
+        }
+    }
+    return 0;
+}
