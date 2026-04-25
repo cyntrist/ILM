8833be1b498e8ce11ffd570aa20fcf7a8610d697
Cynthia Tristán <cyntrist@ucm.es>
2026-04-25 02:20:25 +0200

    Revert "includes"
    This reverts commit ddc462260b285a9148214bb8f31c67e6aa2ad6c7.
    
    Co-Authored-By: Nieves Alonso Gilsanz <nievalon@ucm.es>

---
+++ Raytracing/RayTracer/RayTracer/PointLight.cpp
@@ -0,0 +1,48 @@
+#include "PointLight.h"
+
+#include "glm/geometric.hpp"
+
+PointLight::PointLight(glm::vec3 position, Color color)
+	: _position(position), _color(color)
+{
+    _shadow = true;
+}
+
+Color PointLight::Shade(Ray r, InfoIntersection& hit)
+{
+    Color ret; // color resultante
+
+    //glm::vec3 _direction = hit.p - _position; // direccion desde la luz hasta el punto de colision
+    glm::vec3 _direction = _position - hit.p; // direccion desde la luz hasta el punto de colision
+    
+    float _distance = glm::squared_length(_direction);
+    _direction = glm::normalize(_direction);
+
+    // -- ambiente
+    Color ambient = _color; 
+    glm::vec3 normal = glm::normalize(hit.normal);
+    float lightIntensity = glm::max(0.0f, glm::dot(normal, _direction));
+
+    // -- difusa
+    glm::vec3 diffuse = _color * lightIntensity;
+    glm::vec3 diffuseLighting = (ambient + diffuse);
+
+    // -- especular
+    glm::vec3 view = glm::normalize(_direction);
+    glm::vec3 halfVector = glm::normalize(_direction + view);
+    //glm::vec3 reflect = 2 * glm::dot(normal, _direction) * normal -_direction; // sustituir por halfVector
+
+    float specular = std::max(0.0f, glm::dot(normal, halfVector));
+    //specular = std::pow(specular, 10);
+    glm::vec3 specularColor = _color * specular;
+
+    ret = hit.m->GetColor() * diffuseLighting + specularColor;
+
+    return ret;
+}
+
+glm::vec3 PointLight::ShadowDir(glm::vec3 position)
+{
+    // direccion desde el punto de colision hasta la luz
+    return _position - position;
+}
