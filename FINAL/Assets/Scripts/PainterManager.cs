0af9177b9490093e4ffda8f6b40ed1dc4868b42a
Nieves Alonso Gilsanz <nievalon@ucm.es>
2026-05-05 15:43:39 +0200

    Update PainterManager.cs

--- FINAL/Assets/Scripts/PainterManager.cs
+++ FINAL/Assets/Scripts/PainterManager.cs
@@ -1,9 +1,22 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
+
+struct Paintbrush
+{
+    private string _name;
+
+    Texture2D _brushTexture;
+}
+
 
 public class PainterManager : MonoBehaviour
 {
     [SerializeField]
     private Color mainColor;
+
+    private Color normalColor;
+
+    private Paintbrush mainPaintbrush;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
